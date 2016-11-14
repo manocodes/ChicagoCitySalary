ff3846326847c947cd06557eca251a7b87c71860
manocodes <mano.net@gmail.com>
2016-11-14 16:44:07 -0700

    Final commit before sharing the app in RPubs..

--- ChicagoCitySalary/server.R
+++ ChicagoCitySalary/server.R
@@ -2,12 +2,16 @@ library(shiny)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(data.table)
 
+#a public dataset i retrieved from data.gov which contains salaries of chicago
+#city employees for year 2010 to 2013.
 SalaryData = read.csv("Salary.csv", stringsAsFactors = FALSE)
 
+#here i'm retriving the list of agencies for filtering data
 agencies = table(SalaryData$AgencyTitle)
 
+#using dplyr and tidyr to clean and tidy the data and summarise the data by agency.
+#for example Salary2010 into Year and Salary columns.
 CleanSalaryData = SalaryData %>%
         gather(Year, Salary, Salary2010:Salary2013) %>%
         mutate(Year = as.numeric(gsub("\\D","", Year))) %>%
@@ -16,15 +20,20 @@ CleanSalaryData = SalaryData %>%
 
 shinyServer(function(input, output) {
         
+        #identify the range of years to filter the data.
         minvalue = reactive({input$slider1[1]})
         maxvalue = reactive({input$slider1[2]})
     
+        #dynamically build a dropdown list with agencies.
         output$select <- renderUI({
                 selectInput("select", "Select Agency",  names(agencies))
         })
         
+        #selecting the agency to filter the data, this will be empty in the first load
         agency = reactive({input$select})
     
+        #render the data table. First time it will load all data and second time
+        #we will have to specify an agency..
         output$data = renderTable({    
         CleanSalaryData = CleanSalaryData %>%
                  filter(Year >= as.numeric(minvalue()) & Year <= as.numeric(maxvalue())) 
@@ -38,12 +47,14 @@ shinyServer(function(input, output) {
                 }
         })
         
+        #Title for data
         output$text1 = renderText({
                 label = paste("Salary Datails for Year ", 
                               minvalue(), " to ", 
                               maxvalue() , sep="")
         })
         
+        #plot to compare the salaries when an agency is selected.
         output$plot = renderPlot ({
                 
                 if (!is.null(agency()))
